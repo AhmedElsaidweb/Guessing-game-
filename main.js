7365bfd8dbd787ebcf654c6bdf3af3d17a83f800
Ahmed Elsaid <e2882641@gmail.com>
2026-05-31 19:04:02 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,30 @@
 let num2=parseInt((Math.random())*10) 
 console.log(num2);
 
+let score1=0;
+
+
+
 
 function choose(value){
     if(value==num2){
+        
+        score1+=5;
+        document.getElementById('p').style.display='block';
         document.getElementById('result').innerHTML="لقد فزت"
         document.getElementById('result').style.color="green"
         document.getElementById('div').innerHTML=value;
-     
+         document.getElementById('span').innerHTML=score1;
+    
         
 
     }
 
    else {
-        document.getElementById('result').innerHTML="حاول مرة اخري "
+          
          document.getElementById('result').style.color="red"
+        document.getElementById('result').innerHTML="حاول مرة اخري "
+        
     }
 }
 
