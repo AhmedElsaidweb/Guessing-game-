6d4bb8a6e97ca9d45fba5c6235df515b408363ca
Ahmed Elsaid <e2882641@gmail.com>
2026-06-01 14:54:59 +0300

    Update main2.js

--- main2.js
+++ main2.js
@@ -1,36 +1,42 @@
-let num2=parseInt((Math.random())*10) 
+let num2=parseInt((Math.random())*15) 
 console.log(num2);
 
-let score1=0;
+let score1=5;
+
+function changecolor(button,value){
+ if(value==num2){
+button.style.backgroundColor='green'
+ }
+else{
+    button.style.backgroundColor='red'
+}
 
 
+}
 
-
+let again=5;
 function choose(value){
     if(value==num2){
         
         score1+=5;
-        
+       
         document.getElementById('result').innerHTML="لقد فزت"
         document.getElementById('result').style.color="green"
         document.getElementById('div').innerHTML=value;
          document.getElementById('span').innerHTML=score1;
-     
+     document.getElementById('a').style.display='block'
         
 
     }
 
    else {
-          
+      if(again==0){
+        window.alert('للأسف لقد خسرت')
+    }
+          --again;
+ document.getElementById('again').innerHTML=again;
          document.getElementById('result').style.color="red"
         document.getElementById('result').innerHTML="حاول مرة اخري "
         
     }
-}
-
-//عند الضغط علي السهم تختفي اللعبه ويظهر iframe
-function remove(value){
-    document.getElementById('iframe').style.display='block'; 
-    document.getElementById('iframe').src=value;
-    document.getElementById('header').style.display='none'
 }
