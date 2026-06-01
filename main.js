39363c191825c169d1dedc864db039c3e2702f49
Ahmed Elsaid <e2882641@gmail.com>
2026-06-01 14:54:09 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,14 +3,25 @@ console.log(num2);
 
 let score1=0;
 
+function changecolor(button,value){
+ if(value==num2){
+button.style.backgroundColor='green'
+ }
+else{
+    button.style.backgroundColor='red'
+}
 
 
+}
 
+let again=5;
 function choose(value){
     if(value==num2){
         
+
+
         score1+=5;
-        document.getElementById('p').style.display='block';
+        document.getElementById('a').style.display='block'
         document.getElementById('result').innerHTML="لقد فزت"
         document.getElementById('result').style.color="green"
         document.getElementById('div').innerHTML=value;
@@ -21,15 +32,13 @@ function choose(value){
     }
 
    else {
-          
+    --again;
+    if(again==0){
+        window.alert('للأسف لقد خسرت')
+    }
+ document.getElementById('again').innerHTML=again;
          document.getElementById('result').style.color="red"
         document.getElementById('result').innerHTML="حاول مرة اخري "
         
     }
-}
-
-function remove(value){
-    document.getElementById('iframe').style.display='block'; 
-    document.getElementById('iframe').src=value;
-    document.getElementById('header').style.display='none'
 }
