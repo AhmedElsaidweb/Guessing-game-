2d85fe13a2ab90cbf899c57fbd543abaae581635
Ahmed Elsaid <e2882641@gmail.com>
2026-06-01 14:57:50 +0300

    Add files via upload

---
+++ main5.js
@@ -0,0 +1,41 @@
+let num2=parseInt((Math.random())*30) 
+console.log(num2);
+
+let score1=20;
+
+function changecolor(button,value){
+ if(value==num2){
+button.style.backgroundColor='green'
+ }
+else{
+    button.style.backgroundColor='red'
+}
+
+
+}
+
+let again=5;
+function choose(value){
+    if(value==num2){
+        
+        score1+=5;
+       
+        document.getElementById('result').innerHTML="لقد فزت"
+        document.getElementById('result').style.color="green"
+        document.getElementById('div').innerHTML=value;
+         document.getElementById('span').innerHTML=score1;
+     document.getElementById('a').style.display='block'
+        
+
+    }
+
+   else {  if(again==0){
+        window.alert('للأسف لقد خسرت')
+    }
+          --again;
+ document.getElementById('again').innerHTML=again;
+         document.getElementById('result').style.color="red"
+        document.getElementById('result').innerHTML="حاول مرة اخري "
+        
+    }
+}
