8185045f03bbf3e7a6afda78f91ab39008d1d527
Ahmed Elsaid <e2882641@gmail.com>
2026-06-02 17:59:14 +0300

    Update main2.js

--- main2.js
+++ main2.js
@@ -29,22 +29,22 @@ function choose(value){
 
     }
 
-   else {
-      if(again==0){
+ else {
+    --again;
+    if(again==0){
         window.alert('للأسف لقد خسرت')
         document.getElementById('return').style.display='block';
         document.getElementById('main').style.display='none';
     }
-          --again;
- document.getElementById('again').innerHTML=again;
-        if(value>num2){
+    if(value>num2){
     document.getElementById('result').innerHTML='الرقم السري أقل '
 }
 else if(value<num2){
         document.getElementById('result').innerHTML='الرقم السري أعلي   '
 }
+ document.getElementById('again').innerHTML=again;
          document.getElementById('result').style.color="red"
-     
+
         
     }
 }
