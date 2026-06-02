aa2349a86c291fac13728b6797c0440a54ccd83b
Ahmed Elsaid <e2882641@gmail.com>
2026-06-02 17:48:17 +0300

    Update main5.js

--- main5.js
+++ main5.js
@@ -36,8 +36,14 @@ function choose(value){
     }
           --again;
  document.getElementById('again').innerHTML=again;
+             if(value>num2){
+    document.getElementById('result').innerHTML='الرقم السري أقل '
+}
+else if(value<num2){
+        document.getElementById('result').innerHTML='الرقم السري أعلي   '
+}
          document.getElementById('result').style.color="red"
-        document.getElementById('result').innerHTML="حاول مرة اخري "
+
         
     }
 }
